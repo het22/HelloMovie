23375eee874f1e65be052289b7522bc715d612aa
het99 <gocks0922@gmail.com>
2019-08-21 11:13:30 +0900

    Initial Commit

---
+++ HelloMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloMovie
+//
+//  Created by Het Song on 21/08/2019.
+//  Copyright © 2019 Het Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
