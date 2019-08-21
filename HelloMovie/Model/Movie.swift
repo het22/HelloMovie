e92f7141919f42ed4ca6a060adf98d3a5e43cce5
het99 <gocks0922@gmail.com>
2019-08-21 14:30:32 +0900

    Create Movie.swift
    - 영화의 자료형을 정의한다

---
+++ HelloMovie/Model/Movie.swift
@@ -0,0 +1,21 @@
+//
+//  Movie.swift
+//  HelloMovie
+//
+//  Created by Het Song on 21/08/2019.
+//  Copyright © 2019 Het Song. All rights reserved.
+//
+
+import Foundation
+
+struct Movies: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    let id: Int
+    let poster_path: String
+    let title: String
+    let release_date: String
+    let vote_average: Float
+}
