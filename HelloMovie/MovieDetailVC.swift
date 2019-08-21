04bb458b98eee6a569619267b31d4534572eca32
het99 <gocks0922@gmail.com>
2019-08-21 18:02:02 +0900

    Create MovieDetailVC.swift
    - 영화 상세정보를 보여주는 뷰컨트롤러
    
    - 소스 컨트롤러로 부터 영화 정보를 입력받는다
    
    - 영화 상세정보를 가진다
    - 영화 상세정보가 변경되면 뷰에 반영한다
    
    - 상세정보를 받아서 뷰에 반영하는 메소드 구현

---
+++ HelloMovie/MovieDetailVC.swift
@@ -0,0 +1,44 @@
+//
+//  MovieDetailVC.swift
+//  HelloMovie
+//
+//  Created by Het Song on 21/08/2019.
+//  Copyright © 2019 Het Song. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+    
+    var movie: Movie!
+    var movieDetail: MovieDetail? {
+        didSet { if let detail = movieDetail { configure(detail: detail) } }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var overviewTextView: UITextView!
+    
+    func configure(detail: MovieDetail) {
+        if let path = movie.poster_path {
+            let url = URL(string: "https://image.tmdb.org/t/p/w300/\(path)")
+            posterImageView.sd_setImage(with: url,
+                                        placeholderImage: nil)
+        }
+        titleLabel.text = detail.title
+        subtitleLabel.text = detail.tagline
+        detailLabel.text = "개봉일 \(detail.release_date) | 상영시간 \(detail.runtime)분 | 평점 \(detail.vote_average)점"
+        overviewTextView.text = detail.overview
+    }
+    
+    @IBAction func backButtonTapped() {
+        dismiss(animated: true, completion: nil)
+    }
+}
